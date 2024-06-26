299f249e1b76010981f9302783c2072a4ebeee70
ashikur <ashikur1602@gmail.com>
2024-06-26 12:11:36 +0600

    1. Global Object and Module System

---
+++ index.js
@@ -0,0 +1,6 @@
+const people = require('./people');
+const _ = require("lodash");
+
+console.log(_.last(people.people));
+console.log(people.a);
+console.log(people.test);
---
+++ people.js
@@ -0,0 +1,9 @@
+var people = ['sakib', 'tamim', 'masrafee'];
+
+var a = 5;
+
+function test() {
+    console.log('test');
+}
+
+module.exports = {people, a , test};
