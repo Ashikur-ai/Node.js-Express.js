62f0e6648388ec1ba3f079862bb1ac2d3753bf1d
ashiku2 <ashikur1602@gmail.com>
2024-08-14 07:37:18 +0600

    sending html file as response

--- Hello Express/index.js
+++ Hello Express/index.js
@@ -1,34 +1,25 @@
 const express = require("express");
-
+const fs = require('fs');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send(
-    `
-    <!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <style>
-    .container{
-      width: 50%;
-      margin: 2rem auto;
-      background-color: #ddeeee;
-      padding: 10px;
+  fs.readFile('./pages/index.html', (err, data) => {
+    if (err) {
+      console.log('Error', err);
+      res.send(`<h1>Something went wrong</h1>`);
+    } else {
+      res.write(data);
+      res.end()
     }
-  </style>
-</head>
-<body>
-  <div class="container">
-    <h1>Nodemon is a life saver</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse architecto placeat nulla odio eos deserunt cumque quam mollitia itaque dicta pariatur, modi molestias numquam error nobis laudantium voluptatum voluptatibus est tempora ad? Ratione minus saepe ipsam maiores animi quis. Reiciendis nostrum ex officiis ipsum dolore aliquam, soluta ut laboriosam tempora itaque repellat qui assumenda temporibus quasi, totam et error sed porro minus debitis quas minima nemo hic? Temporibus voluptatem fugiat eaque, saepe veniam, deserunt necessitatibus explicabo dolores sunt voluptatum dolorum pariatur reiciendis error quaerat ea vel sint maxime itaque officia minus culpa totam! Dolorem, ab in labore expedita at maiores.</p>
-  </div>
-</body>
-</html>
-    `
-  )
+  })
+})
+
+app.get('/about', (req, res) => {
+  res.send(`<h1>This is about route</h1>`)
+})
+
+app.get('/help', (req, res) => {
+  res.send(`<h1>This is help route</h1>`)
 })
 
 app.listen(5000, () => {
